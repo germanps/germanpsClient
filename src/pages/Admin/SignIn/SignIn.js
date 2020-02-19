1f0ce91e2bb12349c1b33923e52bca76eca63429
gpla <germanps@outlook.es>
2020-02-19 11:31:57 +0100

    adding tabs to signin component

--- src/pages/Admin/SignIn/SignIn.js
+++ src/pages/Admin/SignIn/SignIn.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import {Layout, Tabs} from 'antd';
+import { Redirect } from "react-router-dom";
+import Logo from '../../../assets/img/png/logo-squalo.png';
 import "./SignIn.scss";
 
 export default function SignIn() {
+    const { Content } = Layout;
+    const { TabPane } = Tabs;
     return(
-        <div>
-            <h1>SignIn</h1>
-        </div>
+        <Layout className="sign-in">
+            <Content className="sign-in__content">
+                <h1 className="sign-in__content-logo">
+                    <img src={Logo} alt="Germán Pla Sepúlveda" />
+                </h1>
+                <div className="sign-in__content-tabs">
+                    <Tabs type="card">
+                        <TabPane tab={<span>Entrar</span>} key="1">
+                            Componente loginForm
+                        </TabPane>
+                        <TabPane tab={<span>Nuevo usuario</span>} key="2">
+                            Component RegisterForm
+                        </TabPane>
+                    </Tabs>
+                </div>
+            </Content>
+        </Layout>
     )
 }
