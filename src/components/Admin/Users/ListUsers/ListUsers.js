2c64704705c6fb13698c0b5f9511a33eeb7891ca
gpla <germanps@outlook.es>
2020-03-04 13:11:15 +0100

    modal component && open edit user modal

--- src/components/Admin/Users/ListUsers/ListUsers.js
+++ src/components/Admin/Users/ListUsers/ListUsers.js
@@ -1,12 +1,16 @@
 import React, {useState} from 'react';
 import { Switch, List, Avatar, Button, Icon } from 'antd';
-import NoAvatar from '../../../../assets/img/png/no-avatar.png'
+import NoAvatar from '../../../../assets/img/png/no-avatar.png';
+import Modal from '../../../Modal';
 import './ListUsers.scss';
 
 export default function ListUsers(props) {
     const { usersActive, usersInactive } = props;
     //states
     const [viewUsersActives, setUsersActives] = useState()
+    const [viewModal, setViewModal] = useState(false);
+    const [modalTitle, setModalTitle] = useState("");
+    const [modalContent, setModalContent] = useState(null);
     
     return(
         <div className="list-users">
@@ -20,17 +24,48 @@ export default function ListUsers(props) {
                 </span>
             </div>
             {viewUsersActives ? ( 
-                <UsersActive usersActive={usersActive}/> 
+                <UsersActive 
+                    usersActive={usersActive} 
+                    setViewModal={setViewModal}
+                    setModalTitle={setModalTitle}
+                    setModalContent={setModalContent}
+                /> 
             ) : ( 
-                <UsersInactives usersInactive={usersInactive} /> 
+                <UsersInactives 
+                    usersInactive={usersInactive} 
+                    setViewModal={setViewModal}
+                    etModalTitle={setModalTitle}
+                    setModalContent={setModalContent}
+                /> 
             )}
+
+            <Modal
+                title={modalTitle}
+                isVisible={viewModal}
+                setIsVisible={() => setViewModal(!viewModal)}
+            >
+                {modalContent}
+            </Modal>
+
         </div>
     )
 }
 
 
 function UsersActive(props) {
-    const {usersActive} = props;
+    const {
+        usersActive, 
+        setViewModal, 
+        setModalTitle, 
+        setModalContent
+    } = props;
+
+    const editUser = user => {
+        setViewModal(true);
+        setModalTitle(`Editar usuario ${user.name ? user.name : "..."} ${user.lastname ? user.lastname : "..."}`);
+        setModalContent(`editando usuario ${user.email}`)
+    }
+
     return (
         <List 
             className="users-active"
@@ -40,19 +75,19 @@ function UsersActive(props) {
                 <List.Item
                     actions={[
                             <Button 
-                                type="primary"
-                                onClick={() => console.log("Editar usuario")}
+                                className="btn-primary"
+                                onClick={() => editUser(user)}
                             >
                                 <Icon type="edit" />
                             </Button>,
                             <Button 
-                                type="danger"
+                                className="btn-warning"
                                 onClick={() => console.log("Desactivar usuario")}
                             >
                                 <Icon type="stop" />
                             </Button>,
                             <Button 
-                                type="danger"
+                                className="btn-danger"
                                 onClick={() => console.log("Borrar usuario")}
                             >
                                 <Icon type="delete" />
@@ -75,7 +110,7 @@ function UsersActive(props) {
 }
 
 function UsersInactives(props) {
-    const {usersInactive} = props;
+    const {usersInactive, setViewModal, setModalTitle, setModalContent} = props;
     return (
         <List 
             className="users-active"
@@ -85,13 +120,14 @@ function UsersInactives(props) {
                 <List.Item
                     actions={[
                             <Button 
-                                type="primary"
-                                onClick={() => console.log("Activar usuario")}
+                                className="btn-primary"
+                                onClick={() => setViewModal(true)}
                             >
                                 <Icon type="check" />
                             </Button>,
                             <Button 
-                                type="danger"
+
+                                className="btn-warning"
                                 onClick={() => console.log("Borrar usuario")}
                             >
                                 <Icon type="delete" />
