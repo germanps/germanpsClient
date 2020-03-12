e757cc8019ed9a61539b40ec76c6894798872c94
gpla <germanps@outlook.es>
2020-03-12 10:31:24 +0100

    menu items draggables

--- src/components/Admin/MenuWeb/MenuWebList/MenuWebList.js
+++ src/components/Admin/MenuWeb/MenuWebList/MenuWebList.js
@@ -1,14 +1,74 @@
 import React, {useEffect, useState} from 'react';
+import { Switch, List, Button, Icon, Modal as AntdModal, notification } from 'antd';
+import Modal from '../../../Modal/Modal';
+import DragSortableList from 'react-drag-sortable';
+import "./MenuWebList.scss";
 
+const { confirm } = AntdModal;
 
 export default function MenuWebList(props){
     const { menu, setReloadMenu } = props;
 
+    //states
+    const [listItems, setListItems] = useState([])
+    const [isVisibleModal, setIsVisibleModal] = useState(false);
+    const [modalTitle, setModalTitle] = useState("");
+    const [modalContent, setModalContent] = useState(null);
+
+    useEffect(() => {
+        const listItemsArray = [];
+
+        menu.forEach(item => {
+            listItemsArray.push({
+                content: (
+                    <MenuItem item={item} />
+                )
+            })
+        });
+
+        setListItems(listItemsArray);
+    }, [menu]);
+
+
+    const onSort = (sortedList, dropEvent) => {
+        console.log(sortedList);
+        
+    }
+
     return (
+
         <div className="menu-web-list">
-            {menu.map(item => (
-                <p key={item._id}>{item.title}</p>
-            ))}
+            <div className="menu-web-list__header">
+                <Button type="primary">
+                    Menu menú
+                </Button>
+            </div>
+            <div className="menu-web-list__items">
+                <DragSortableList 
+                    items={listItems}
+                    onSort={onSort}
+                    type="vertical"
+                />
+            </div>
         </div>
     );
+}
+
+function MenuItem(props) {
+    const { item } = props;
+    return(
+        <List.Item
+            actions={[
+                <Switch defaultChecked={item.active} />,
+                <Button type="primary">
+                    <Icon type="edit" />
+                </Button>,
+                <Button type="danger">
+                    <Icon type="delete" />
+                </Button>
+            ]}
+        >
+            <List.Item.Meta title={item.title} description={item.url} />
+        </List.Item>
+    )
 }
