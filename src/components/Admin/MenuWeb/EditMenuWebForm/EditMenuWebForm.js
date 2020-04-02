51352a627dc7d8f57efac7698a79349b24c71904
gpla <germanps@outlook.es>
2020-04-02 13:29:00 +0200

    update menuWeb in BBDD

--- src/components/Admin/MenuWeb/EditMenuWebForm/EditMenuWebForm.js
+++ src/components/Admin/MenuWeb/EditMenuWebForm/EditMenuWebForm.js
@@ -5,35 +5,68 @@ import { getAccessTokenApi } from '../../../../api/auth';
 import './EditMenuWebForm.scss';
 
 export default function EditMenuWebForm(props){
-    const { setIsVisibleModal, setRealoadMenuWeb, menu } = props;
+    const { setIsVisibleModal, setReloadMenu, menu } = props;
+    const [ menuWebData, setMenuWebData] = useState({});
+
+    useEffect(() => {
+        setMenuWebData(menu);
+    }, [menu]) //se actualiza cada vez que se modifique el menú
+
+    const editMenu = e => {
+        e.preventDefault();
+
+        if (!menuWebData.title || !menuWebData.url) {
+            notification["error"]({
+                message: "Todos los campos son obligatorios"
+            });
+        }else{
+            const accessToken = getAccessTokenApi();
+            updateMenuApi(accessToken, menuWebData._id, menuWebData)
+                .then(response => {
+                    notification["success"]({
+                        message: response 
+                    });
+                    setIsVisibleModal(false);
+                    setReloadMenu(true);
+                }).catch(() => {
+                    notification["error"]({
+                        message: "Error del servidor, intentarlo mas tarde."
+                    })
+                })
+        }
+        
+    }
 
     return (
         <div className="edit-menu-web-form">
-            <EditForm />
+            <EditForm 
+                menuWebData={menuWebData}
+                setMenuWebData={setMenuWebData}
+                editMenu={editMenu}
+            />
         </div>
     )
 }
 
 
 function EditForm(props) {
-    // const { menuWebData, setMenuWebData, editMenu, menu } = props;
+    const { menuWebData, setMenuWebData, editMenu } = props;
     return(
-        <Form className="form-edit" /*onSubmit={editMenu}*/>
+        <Form className="form-edit" onSubmit={editMenu}>
             <Form.Item>
                 <Input 
                     prefix={<Icon type="font-size" />}
                     placeholder="Título"
-                    //value={menuWebData.title}
-                    //onChange={e => setMenuWebData({ ...menuWebData, title: e.target.value })}
+                    value={menuWebData.title}
+                    onChange={e => setMenuWebData({ ...menuWebData, title: e.target.value })}
                 />
             </Form.Item>
             <Form.Item>
                 <Input 
-                    //addonBefore={selectBefore}
                     prefix={<Icon type="link" />}
                     placeholder="URL"
-                    //value={menuWebData.url}
-                    //onChange={e => setMenuWebData({ ...menuWebData, url: e.target.value })}
+                    value={menuWebData.url}
+                    onChange={e => setMenuWebData({ ...menuWebData, url: e.target.value })}
                 />
             </Form.Item>
             <Form.Item>
