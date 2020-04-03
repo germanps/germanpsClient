da2068a3567ae3f7fa22183c4a745898b033bc66
gpla <germanps@outlook.es>
2020-04-03 07:18:16 +0200

    basic layout colums

--- src/layouts/LayoutBasic.js
+++ src/layouts/LayoutBasic.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { Layout } from "antd";
+import { Layout, Row, Col } from "antd";
 import "./LayoutBasic.scss";
 
 export default function LayoutBasic(props) {
@@ -8,19 +8,32 @@ export default function LayoutBasic(props) {
     const { routes } = props;
     const { Content, Footer } = Layout;
 
+    // return(
+    //     <Layout>
+    //         <h2>Menu...</h2>
+    //         <Layout>
+    //             <Content>
+    //                 <LoadRoutes routes={routes} />
+    //             </Content>
+    //             <Footer>
+    //                 Germán Pla 
+    //             </Footer>
+    //         </Layout>
+    //     </Layout>
+    // );
     return(
-        <Layout>
-            <h2>Menu...</h2>
-            <Layout>
-                <Content>
-                    <LoadRoutes routes={routes} />
-                </Content>
+        <Row>
+            <Col lg={4} />
+            <Col lg={16}>
+                <p>MenuTop</p>
+                <LoadRoutes routes={routes} />
                 <Footer>
-                    Germán Pla 
-                </Footer>
-            </Layout>
-        </Layout>
-    );
+                     Germán Pla 
+                 </Footer>
+            </Col>
+            <Col lg={4} />
+        </Row>
+    )
 }
 
 function LoadRoutes({routes}) {
